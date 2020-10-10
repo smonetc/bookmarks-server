648b2208dccab5193115d6881e34d7087670205a
Monet Cooper <smomoc@Monets-MacBook-Pro-2.local>
2020-10-10 01:33:09 -0400

    updated test

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -49,6 +49,38 @@ describe('Get /bookmarks Endpoint', function (){
     })
 })
 
+describe('Get /bookmarks/:bookmark_id Endpoint', function (){
+    context('Given no bookmarks', () => {
+
+        it(`responds with 404 when bookmark doesn't exist`, () => {
+            return supertest(app)
+              .get('/bookmarks/123')
+              .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+              .expect(404, {
+                error: {message: 'Bookmark Not Found'}
+              })
+          })
+    })
+    context('Given there are bookmarks in the database', () => {
+        const testBookmarks = makeBookmarkArray()
+
+        beforeEach('insert bookmarks', () => {
+            return db
+                .into('bookmarks')
+                .insert(testBookmarks)
+            })
+
+            it('GET /bookmarks/:bookmark_id responds with 200 and the specified bookmark', () => {
+                const bookmarkId = 2
+                const expectedBookmark = testBookmarks[bookmarkId - 1]
+                return supertest(app)
+                .get(`/bookmarks/${bookmarkId}`)
+                .set('Authorization', `Bearer ${process.env.API_TOKEN}`)
+                .expect(200, expectedBookmark)
+            })
+    })
+})
+
 
 
 
